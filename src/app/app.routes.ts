3ff25472b8f54e856ad02766ab4dc07a88a7a81c
YasuoArcher <marlon25.masabanda@gmail.com>
2024-12-22 18:47:54 -0500

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CardComponent } from './shared/card/card.component';
+
+export const routes: Routes = [
+    {path: 'card', component: CardComponent}
+];
