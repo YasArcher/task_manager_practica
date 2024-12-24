e9093e72fe0553027f4856b4d017f9e5a62ec010
YasuoArcher <marlon25.masabanda@gmail.com>
2024-12-24 09:06:27 -0500

    arreglado

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://vgnnieizrwmjemlnziaj.supabase',
+  supabaseUrl: 'https://vgnnieizrwmjemlnziaj.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnbm5pZWl6cndtamVtbG56aWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwMDA2MDIsImV4cCI6MjAyNzU3NjYwMn0.ItXBN_QYXZsrJL_cJdPWumCxsISYxGzTatcn4hysWOI',
 }
